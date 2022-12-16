97ca98263c5658b6db7ad7f09fe1eff6e9434b2c
daejung90 <djung.ca@gmail.com>
2022-12-16 18:04:36 -0500

    adding getAllReservations query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -38,7 +38,7 @@ const getUserWithEmail = (email) => {
   .query (`SELECT * FROM users WHERE email = $1`, [email])
   .then((result) => {
     console.log(result.rows);
-    return result.rows;
+    return result.rows[0];
   })
   .catch((err) => {
     console.log(err.message)
@@ -62,7 +62,7 @@ const getUserWithId = (id) => {
   .query(`SELECT * FROM users WHERE id = $1`, [id])
   .then((result) => {
     console.log(result.rows)
-    return result.rows;
+    return result.rows[0];
   })
   .catch((err) => {
     return err.message;
@@ -104,10 +104,39 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
+// const getAllReservations = function(guest_id, limit = 10) {
+//   return getAllProperties(null, 2);
+// }
+// exports.getAllReservations = getAllReservations;
+
+const getAllReservations = (guest_id, limit = 10) => {
+  return pool
+  .query(`SELECT reservations.*, properties.*, avg(rating) as average_rating
+          FROM reservations
+          JOIN properties ON reservations.property_id = properties.id
+          JOIN property_reviews ON properties.id = property_reviews.property_id
+          WHERE reservations.guest_id = $1
+          GROUP BY properties.id, reservations.id
+          ORDER BY reservations.start_date
+          LIMIT = $2`, [guest_id, limit])
+  .then((result) => {
+    console.log(result.rows);
+    return result.rows
+  })
+  .catch((err) => {
+    return err.message
+  })          
+};
 exports.getAllReservations = getAllReservations;
+
+// SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
+// FROM reservations
+// JOIN properties ON reservations.property_id = properties.id
+// JOIN property_reviews ON properties.id = property_reviews.property_id
+// WHERE reservations.guest_id = 1
+// GROUP BY properties.id, reservations.id
+// ORDER BY reservations.start_date
+// LIMIT 10;
 
 /// Properties
 
